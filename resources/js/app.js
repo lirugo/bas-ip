62f986524b9537027a208e6b5e03005856bb73da
lirugo <roostik21@gmail.com>
2019-01-14 16:55:25 +0200

    Added dependencies - Vue,Vuetify,Vue-router,MaterialIcons,lodash with helping yarn Implemented base structure for views, and added main page with static table

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,43 @@
 
-/**
- * First, we will load all of this project's Javascript utilities and other
- * dependencies. Then, we will be ready to develop a robust and powerful
- * application frontend using useful Laravel and JavaScript libraries.
- */
-
 require('./bootstrap');
+
+import _ from 'lodash'
+
+//Material icons
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+//Vue
+import Vue from 'vue'
+
+//Vuetify
+import Vuetify from 'vuetify'
+Vue.use(Vuetify);
+
+//Vuetify styles
+import 'vuetify/dist/vuetify.min.css'
+
+//VueRouter
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//Imports components
+import WidgetEmployeeList from './components/employee/List'
+
+//Routes
+const routes = [
+    //Employees
+    { path: '/', component: WidgetEmployeeList},
+
+]
+
+const router = new VueRouter({
+    routes
+})
+
+//Main
+new Vue({
+    el: '#app',
+    components: {
+    },
+    router
+}).$mount('#app');
---
+++ resources/views/_layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- App Name -->
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    @yield('styles')
+</head>
+<body>
+
+    <v-app id="app">
+        <!-- Main Navigation -->
+        @include('_includes.nav.toolbar')
+
+        <!-- Content -->
+        @yield('content')
+    </v-app>
+
+<!-- Default Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+<!-- Scripts From Blade -->
+@yield('scripts')
+
+</body>
+</html>
